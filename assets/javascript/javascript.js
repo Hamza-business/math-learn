7084d1ca26b171f0d4355c7517c8ae9b84f87087
Hamza Elkotb <97664403+Hamza-business@users.noreply.github.com>
2022-09-26 16:22:09 +0300

    pay pages making

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -52,6 +52,40 @@ setTimeout(() => {
 }, 10000);
 
 
+function payWayShow(){
+    let clicko = document.getElementById("selectedShow");
+    clicko.classList.toggle("active");
+    let iconro = document.getElementById("iconro");
+    iconro.classList.toggle("active");
+    let paySection = document.getElementById("section");
+    paySection.classList.toggle("active");
+}
+
+function payouts(){
+    try{
+        let clicko = document.getElementById("selectedShow");
+        let payo = document.getElementById("payWay");
+        let ta = document.getElementById("taxs");
+        let elese = document.querySelectorAll(".paywayo");
+        elese.forEach((ele)=>{
+            ele.addEventListener("click",()=>{
+                let pa1 = ele.querySelectorAll(".togeto")[0];
+                let ta1 = ele.querySelectorAll(".togeto")[1];
+                payo.innerHTML = pa1.textContent
+                ta.innerHTML = ta1.textContent
+                elese.forEach((el)=>{
+                    el.classList.remove("active")
+                })
+                ele.classList.add("active")
+            })
+        })
+    }catch(e){
+
+    }
+}
+payouts()
+
+
 
 function questionChecker(){
     try{
